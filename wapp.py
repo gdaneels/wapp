86d99b0c5bed4fb5fd82550eb728fd29447f6143
Glenn Daneels <glenndaneels@gmail.com>
2023-09-20 22:41:32 +0200

    Included parsing for throughput parsing with milli- and microseconds.

--- wapp.py
+++ wapp.py
@@ -15,7 +15,7 @@ def parse_timestamp(line):
     timestamp_pattern = re.compile("\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}")
     match = timestamp_pattern.search(line)
     if match:
-        return datetime.strptime(str(matcher.group(0)), '%Y-%m-%d %H:%M:%S')
+        return datetime.strptime(str(match.group(0)), '%Y-%m-%d %H:%M:%S')
     else:
         # print("Didn't find correct timestamp in line: {0}".format(line.strip()))
         return None
@@ -38,11 +38,40 @@ def parse_virtual_memory_usage(line):
     mem_usage = float(str_mem_usage.split("%")[0])
     return mem_usage
 
+def parse_mbps_milliseconds(line):
+    pattern = re.compile("Throughput (\d+) bytes in the last (\d+) milliseconds.")
+    is_match = pattern.search(line)
+    if (is_match):
+        bytes = float(is_match.group(1))
+        ms = float(is_match.group(2))
+        mbps = ((bytes * 8.0 / ms) * 1000.0) / 1000.0 / 1000.0
+        # print(f"bytes = {bytes}, ms = {ms}, mbps = {mbps}")
+        return mbps
+    else:
+        return None
+
+def parse_mbps_microseconds(line):
+    pattern = re.compile("Throughput (\d+) bytes in the last (\d+) microseconds.")
+    is_match = pattern.search(line)
+    if (is_match):
+        bytes = float(is_match.group(1))
+        ms = float(is_match.group(2))
+        mbps = ((bytes * 8.0 / ms) * 1000000.0) / 1000.0 / 1000.0
+        # print(f"bytes = {bytes}, ms = {ms}, mbps = {mbps}")
+        return mbps
+    else:
+        return None
+
 def parse(data_file):
     with open(data_file, errors='ignore') as f:
         for line in f:
-            # timestamp = parse_timestamp(line)
+            timestamp = parse_timestamp(line)
             # print(timestamp)
+            # mbps = parse_mbps_milliseconds(line)
+            # mbps = parse_mbps_microseconds(line)
+            # if mbps:
+            #     parsed_data.append({"timestamp": timestamp, "file": data_file, "metric": "mbps", "value": mbps})
+            # print(mbps)
             cpu_usage = parse_cpu(line)
             parsed_data.append({"timestamp": None, "file": data_file, "metric": "cpu_usage", "value": cpu_usage})
             virtual_memory_usage = parse_virtual_memory_usage(line)
@@ -78,5 +107,5 @@ if __name__ == "__main__":
 
     # first convert it to a Pandas dataframe for easy manipulation and plotting
     df = pd.DataFrame.from_records(parsed_data)
-    print(df.to_string())
-    generate_report(df, current_timestamp)
+    # print(df.to_string())
+    generate_report(df.drop('timestamp', axis=1, inplace=False), current_timestamp)
