18ba99c3fc1ba4efb46e175bb9239851fc1c7a9a
Glenn Daneels <glenndaneels@gmail.com>
2023-09-24 09:10:28 +0200

    Introduced different classes.

---
+++ parser.py
@@ -0,0 +1,7 @@
+class Parser:
+   
+    def generate(self, dataframe):
+       pass 
+
+    def __init__(self, output_dir=None):
+        print("Parser class initialized.")
---
+++ report.py
@@ -0,0 +1,9 @@
+class Report:
+   
+    def generate(self, dataframe):
+       pass 
+
+    def __init__(self, output_dir=None):
+        self.summary_name = "summary"
+        self.data_name = "data"
+        print("Report class initialized.")
