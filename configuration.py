54aafa46f91ef8b428dd91d9fff0412c22d23ef7
Glenn Daneels <glenndaneels@gmail.com>
2023-09-25 11:45:54 +0200

    Checking data files in configuration now. Added start for Parser parse functionality.

--- configuration.py
+++ configuration.py
@@ -1,22 +1,43 @@
 import json
+import os
 
 class Configuration:
     def __init__(self, path_configuration_file=""):
         print("Initializing Configuration.")
+
+        if not os.path.exists(path_configuration_file):
+            raise Exception(f"Input JSON configuration {path_configuration_file} does not exist.")
+
         self.configuration = self._parse(path_configuration_file)
+
         print("Parsed configuration file.")
+
+    def _parse_paths_data(self, configuration):
+        data_files = configuration.keys()
+        for file in data_files:
+            if not os.path.exists(file):
+                raise Exception(f"Data file {file} does not exist.")
 
     def _parse(self, path_configuration_file=""):
         configuration = None
         with open(path_configuration_file) as json_file:
             configuration = json.load(json_file)
+
+        if "metrics" not in configuration:
+            raise Exception("Missing metrics part in configuration.")
+
+        # check if all data paths exists
+        self._parse_paths_data(configuration["metrics"])
+
         return configuration
 
     def get(self):
         return self.configuration
 
-    def metrics(self):
+    def data(self):
         return self.configuration["metrics"]
 
     def plots(self):
-        return self.configuration["plots"]
+        if "plots" in self.configuration:
+            return self.configuration["plots"]
+        return None
--- parser.py
+++ parser.py
@@ -1,7 +1,33 @@
-class Parser:
-   
-    def generate(self, dataframe):
-       pass 
+import json
 
-    def __init__(self, output_dir=None):
-        print("Parser class initialized.")
+class Parser:
+    def __init__(self, data=None):
+        print("Initializing Parser.")
+        if data is None:
+            raise Exception("Parser should be given a data argument to stored parsed data.")
+        self.data = data
+
+    def parse(self, configuration_data):
+        data_files = configuration_data.keys()
+        for data_file in data_files:
+            self.parse_data_file()
+
+    def parse_data_file(self, path, parse_configurations):
+        print(f"Parsing file {path} with following configuration:")
+        print(json.dumps(parse_configurations, indent=4))
+        with open(path, errors='ignore') as f:
+            for line in f:
+                for config in parse_configurations:
+                    # parse timestamp
+                    timestamp = None
+                    if "parse_timestamp" in config:
+                        parse_timestamp = config["parse_timestamp"]
+                        timestamp = getattr(parse_functions, parse_timestamp)(line)
+
+                    # parse metric
+                    parse_function = config["parse_function"]
+                    metric = config["metric"]
+                    metric_val = getattr(parse_functions, parse_function)(line)
+                    if metric_val:
+                        parsed_data.append({"timestamp": timestamp, "file": path, "metric": metric, "value": metric_val})
+        print("Parsing done.")
