08b4399ea2c4bff8bd7b80d28ef0768161e2ffbd
Glenn Daneels <glenndaneels@gmail.com>
2023-09-25 11:45:54 +0200

    Checking data files in configuration now. Added start for Parser parse functionality.

--- wapp.py
+++ wapp.py
@@ -13,22 +13,23 @@ class Wapp:
         self.name_output_dir = "output"
         self.name_reports_dir = self.name_output_dir + "/" + "reports"
         self.name_plots_dir = self.name_output_dir + "/" + "plots"
-
-        if not os.path.exists(path_configuration_file):
-            raise Exception(f"Input JSON configuration {path_configuration_file} does not exist.")
         self.path_configuration_file = path_configuration_file
-        self.path_output_dir = self._get_output_dir()
-        print(f"Created output dir {self.path_output_dir}.")
+        self.path_output_dir = self._make_output_dir()
 
         self.configuration = Configuration(path_configuration_file)
+        self.parser = Parser()
 
-    def _get_output_dir(self):
+    def _make_output_dir(self):
         path_output_dir = self.name_output_dir + "/" + os.path.basename(self.path_configuration_file).split(".")[0]
         if not os.path.exists(path_output_dir):
             os.makedirs(path_output_dir)
+            print(f"Created output dir {self.path_output_dir}.")
         else:
             raise Exception(f"Output directory {path_output_dir} exists already.")
         return path_output_dir
+
+    def parse(self):
+        self.parser.parse(self.configuration.data())
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
