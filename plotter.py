dcff624bef35f664fac33db72af67b0864087970
Glenn Daneels <glenndaneels@gmail.com>
2023-09-25 23:11:30 +0200

    Finished basic Plotter functionality to plot individual metrics.

--- plotter.py
+++ plotter.py
@@ -1,3 +1,6 @@
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
 import os
 
 class Plotter:
@@ -13,5 +16,46 @@ class Plotter:
         self.configuration_data = configuration_data
         self.dataframe = dataframe
 
+    def _need_plot(self, metric_parse_configuration):
+        return ("plot" in metric_parse_configuration and (metric_parse_configuration["plot"] == 1 or isinstance(metric_parse_configuration["plot"], str)))
+
+    def _get_plot_name(self, metric_parse_configuration):
+        plot_name = metric_parse_configuration["name"]
+        if isinstance(metric_parse_configuration["plot"], str):
+            plot_name = metric_parse_configuration["plot"]
+        return plot_name
+
+    def _generate_plot(self, plot_name, data_file, metric):
+        path_plot = self.plots_dir + "/" + plot_name
+
+        # save all data
+        df_metric = self.dataframe.loc[(self.dataframe["file"] == data_file) & (self.dataframe["metric"] == metric)]
+
+        # make the plot
+        plt.figure(figsize=(20, 5))
+        sns.lineplot(df_metric["value"], linewidth=1, marker=".", label=path_plot)
+        plt.title("{0} over time".format(metric))
+        plt.ylabel("{0}".format(metric))
+        plt.xlabel('measurement')
+        plt.legend(loc="upper left")
+        plt.tight_layout()
+        plt.savefig(path_plot)
+
+    def _generate_plot_metric(self, data_file, metric_parse_configuration):
+        if not self._need_plot(metric_parse_configuration):
+            return
+        
+        plot_name = self._get_plot_name(metric_parse_configuration)
+        metric = metric_parse_configuration["metric"]
+        # parse the data frame for the metric and data file you want to be reported
+        self._generate_plot(plot_name, data_file, metric)
+        print(f"Generated plot for metric \"{metric}\" in data file \"{data_file}\".")
+
     def generate(self):
-        pass
+        if self.dataframe is None:
+            raise Exception("No data(frame) yet to generate plots from.")
+
+        data_files = self.configuration_data.keys()
+        for data_file in data_files:
+            for metric_parse_configuration in self.configuration_data[data_file]:
+                self._generate_plot_metric(data_file, metric_parse_configuration)
