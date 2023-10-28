39bb5703cd44ce734a903a4e34d9f28aef5da87a
gdaneels <glenndaneels@gmail.com>
2023-10-28 23:20:47 +0200

    Adds parse_timestamp start and stop and bundling in one dictionary.

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,5 @@
 from src import wapp_functions
+from datetime import datetime
 
 class Parser:
     def __init__(self, parsed_data=None):
@@ -7,14 +8,36 @@ class Parser:
             raise Exception("Parser should be given a data argument to stored parsed data.")
         self.parsed_data = parsed_data
 
+    def _parse_valid_timestamp(self, metric_parse_config, line):
+        # parse timestamp
+        timestamp = None
+        timestamp_start = None
+        timestamp_stop = None
+        timestamp_within_range = True
+        if "parse_timestamp" in metric_parse_config:
+            # no need to check for "function" presence as this is checked by Configuration class
+            parse_timestamp = metric_parse_config["parse_timestamp"]["function"]
+            timestamp = getattr(wapp_functions, parse_timestamp)(line)
+            if timestamp is None:
+                return None, False
+            if "start" in metric_parse_config["parse_timestamp"]:
+                timestamp_start = metric_parse_config["parse_timestamp"]["start"]
+                timestamp_start = datetime.strptime(timestamp_start, '%H:%M:%S')
+                if timestamp < timestamp_start:
+                    timestamp_within_range = False
+            if "stop" in metric_parse_config["parse_timestamp"]:
+                timestamp_stop = metric_parse_config["parse_timestamp"]["stop"]
+                timestamp_stop = datetime.strptime(timestamp_stop, '%H:%M:%S')
+                if timestamp > timestamp_stop:
+                    timestamp_within_range = False
+        return timestamp, timestamp_within_range
+
     def _parse_line(self, data_file, data_file_parse_config, line):
         # a parse configuration per data file can have multiple parse configurations per metric
         for metric_parse_config in data_file_parse_config:
-            # parse timestamp
-            timestamp = None
-            if "parse_timestamp" in metric_parse_config:
-                parse_timestamp = metric_parse_config["parse_timestamp"]
-                timestamp = getattr(wapp_functions, parse_timestamp)(line)
+            timestamp, timestamp_within_range = self._parse_valid_timestamp(metric_parse_config, line)
+            if timestamp is not None and not timestamp_within_range:
+                return
             # parse metric
             parse_function = metric_parse_config["parse_function"]
             parse_function_arg = None
@@ -27,11 +50,11 @@ class Parser:
             if metric_val is not None:
                  self.parsed_data.append({"timestamp": timestamp, "file": data_file, "metric": metric, "value": metric_val})
 
-    def _parse_data_file(self, data_file, data_file_parse_config):
+    def _parse_data_file(self, data_file, data_file_metric_configurations):
         # print(json.dumps(data_file_parse_config, indent=4))
         with open(data_file, errors='ignore') as f:
             for line in f:
-                self._parse_line(data_file, data_file_parse_config, line)
+                self._parse_line(data_file, data_file_metric_configurations, line)
             print(f"Parsed file {data_file}.")
 
     def parse(self, configuration_data):
