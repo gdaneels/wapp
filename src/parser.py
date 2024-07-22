4c6e46a42cd9b1b6f850ff596479a47a864bb897
Glenn Daneels <glenndaneels@gmail.com>
2024-07-22 14:18:12 +0200

    Implemented parsing and plotting of events.

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,5 @@
 from src import wapp_functions
+from src import wapp_event_functions
 from datetime import datetime
 
 class Parser:
@@ -23,12 +24,12 @@ class Parser:
             if "start" in metric_parse_config["parse_timestamp"]:
                 timestamp_start = metric_parse_config["parse_timestamp"]["start"]
                 timestamp_start = datetime.strptime(timestamp_start, '%H:%M:%S')
-                if timestamp < timestamp_start:
+                if timestamp.time() < timestamp_start.time():
                     timestamp_within_range = False
             if "stop" in metric_parse_config["parse_timestamp"]:
                 timestamp_stop = metric_parse_config["parse_timestamp"]["stop"]
                 timestamp_stop = datetime.strptime(timestamp_stop, '%H:%M:%S')
-                if timestamp > timestamp_stop:
+                if timestamp.time() > timestamp_stop.time():
                     timestamp_within_range = False
         return timestamp, timestamp_within_range
 
@@ -36,8 +37,9 @@ class Parser:
         # a parse configuration per data file can have multiple parse configurations per metric
         for metric_parse_config in data_file_parse_config:
             timestamp, timestamp_within_range = self._parse_valid_timestamp(metric_parse_config, line)
-            if timestamp is not None and not timestamp_within_range:
+            if timestamp is None or (timestamp is not None and not timestamp_within_range):
                 return
+
             # parse metric
             parse_function = metric_parse_config["parse_function"]
             parse_function_arg = None
@@ -48,7 +50,13 @@ class Parser:
             else:
                 metric_val = getattr(wapp_functions, parse_function)(line)
             if metric_val is not None:
-                 self.parsed_data.append({"timestamp": timestamp, "file": data_file, "metric": metric, "value": metric_val})
+                self.parsed_data.append({"timestamp": timestamp, "file": data_file, "metric": metric, "value": metric_val, "event": None})
+
+            # parse event
+            if "parse_events" in metric_parse_config:
+                for event_name, parse_event_function in metric_parse_config["parse_events"].items():
+                    if getattr(wapp_event_functions, parse_event_function)(line):
+                        self.parsed_data.append({"timestamp": timestamp, "file": data_file, "event": event_name, "value": None, "metric": metric})
 
     def _parse_data_file(self, data_file, data_file_metric_configurations):
         # print(json.dumps(data_file_parse_config, indent=4))
